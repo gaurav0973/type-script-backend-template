f39fa21661559ec541dd30828710d88acf4d4a45
gaurav0973 <codejourneypanda@gmail.com>
2026-03-12 13:11:03 +0530

    Express server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello from here")
+import http from "http"
+import { env } from "./env.js"
+import {createServerApplication} from "./app/index.js"
+
+
+async function main(){
+    try {
+        const server = http.createServer(createServerApplication)
+        const PORT:number = env.PORT ? +env.PORT : 8080
+
+        server.listen(PORT, ()=>{
+            console.log(`Server is running on PORT: ${PORT} : 🛞`)
+        })
+    } catch (error) {
+        throw new Error("Server is not able to run")
+    }
+}
+
+main()
