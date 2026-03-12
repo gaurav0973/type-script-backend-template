f318d2ca5b782b10fd31bef6506ffa96621365f0
gaurav0973 <codejourneypanda@gmail.com>
2026-03-12 13:42:06 +0530

    simple api endpoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,5 +3,14 @@ import type { Application } from "express"
 
 export function createServerApplication(): Application{
     const app = express()
+
+    app.get("/home", function(req, res){
+        return res.json({
+            message : "This is how I handle the routes",
+            statusCode: 200
+        })
+    })
+
+
     return app
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import http from "http"
-import { env } from "./env.js"
-import {createServerApplication} from "./app/index.js"
+import http from "http";
+import { env } from "./env.js";
+import { createServerApplication } from "./app/index.js";
 
+async function main() {
+  try {
+    const app = createServerApplication();
+    const server = http.createServer(app);
+    const PORT: number = env.PORT ? +env.PORT : 8080;
 
-async function main(){
-    try {
-        const server = http.createServer(createServerApplication)
-        const PORT:number = env.PORT ? +env.PORT : 8080
-
-        server.listen(PORT, ()=>{
-            console.log(`Server is running on PORT: ${PORT} : 🛞`)
-        })
-    } catch (error) {
-        throw new Error("Server is not able to run")
-    }
+    server.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT} : 🛞`);
+    });
+  } catch (error) {
+    throw new Error("Server is not able to run");
+  }
 }
 
-main()
+main();
